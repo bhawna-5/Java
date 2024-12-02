24d8ea49f9d363d9da9a9c6a1b5112d4f5205522
bhawna-5 <bhawnagupta4545@gmail.com>
2024-12-03 00:32:58 +0530

    first commit

--- Data Structures & Algorithms/src/linkedList/SinglyLL.java
+++ Data Structures & Algorithms/src/linkedList/SinglyLL.java
@@ -15,18 +15,72 @@ public class SinglyLL {
     }
 
     public void addfirst(int data) {
-        node newNode=new node(data);
-        if (head==null){
-            head=newNode;
-        }
-        else {
-            newNode.next=head;
-            head=newNode;
+        node newNode = new node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
         }
     }
 
+    void addLast(int data) {
+        node newNode = new node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    void print() {
+        node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + "->");
+            curr = curr.next;
+        }
+        System.out.print("null");
+    }
+
+    void deleteFirst() {
+        if (head == null) System.out.println("empty list");
+        head = head.next;
+    }
+
+    void deleteLast() {
+        if (head == null) {
+            System.out.println("Empty List, nothing to delete");
+            return;
+        }
+
+
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+        node secondLast = head;
+        node last = head.next;
+        while (last.next != null) {
+            last = last.next;
+            secondLast = secondLast.next;
+        }
+        secondLast.next = null;
+    }
 
     public static void main(String[] args) {
-
+        SinglyLL ll = new SinglyLL();
+        ll.addfirst(10);
+        ll.addLast(70);
+        ll.addLast(80);
+        ll.addLast(90);
+        ll.addLast(20);
+        ll.print();
+        ll.deleteFirst();
+        System.out.println();
+        ll.print();
     }
 }
