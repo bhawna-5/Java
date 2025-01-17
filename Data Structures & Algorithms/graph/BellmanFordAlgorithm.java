f5225f1f392b45eacedbc08c45931c08361aac27
bhawna-5 <bhawnagupta4545@gmail.com>
2025-01-17 23:16:36 +0530

    first commit

--- Data Structures & Algorithms/graph/BellmanFordAlgorithm.java
+++ Data Structures & Algorithms/graph/BellmanFordAlgorithm.java
@@ -1,6 +1,29 @@
 package graph;
 
+import java.util.ArrayList;
+
 public class BellmanFordAlgorithm {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for(int i=0; i<graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
+        graph[1].add(new Edge(1, 2, -4));
+        graph[2].add(new Edge(2, 3, 2));
+        graph[3].add(new Edge(3, 4, 4));
+        graph[4].add(new Edge(4, 1, -1));
+    }
     public static void main(String[] args) {
 
     }
