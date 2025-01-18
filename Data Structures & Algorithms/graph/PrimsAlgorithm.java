9a90494affd042c2234d0cee7577f7047e61a005
bhawna-5 <bhawnagupta4545@gmail.com>
2025-01-18 12:43:08 +0530

    first commit

--- Data Structures & Algorithms/graph/PrimsAlgorithm.java
+++ Data Structures & Algorithms/graph/PrimsAlgorithm.java
@@ -1,7 +1,34 @@
 package graph;
 
 public class PrimsAlgorithm {
+    static class Edge {
+        int src;
+        int dest;
+        int weight;
+
+        public Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = wt;
+        }
+    }
+
+    static class Pair implements Comparable<Pair> {
+        int n;
+        int path;
+
+        public Pair(int n, int path) {
+            this.n = n;
+            this.path = path;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return this.path - p2.path;
+        }
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
