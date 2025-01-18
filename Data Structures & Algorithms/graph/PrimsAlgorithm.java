f4db6e8b68823786778d02de31f31dfaf18c19ca
bhawna-5 <bhawnagupta4545@gmail.com>
2025-01-18 15:54:26 +0530

    first commit

--- Data Structures & Algorithms/graph/PrimsAlgorithm.java
+++ Data Structures & Algorithms/graph/PrimsAlgorithm.java
@@ -49,27 +49,66 @@ public class PrimsAlgorithm {
         graph[3].add(new Edge(3, 2, 50));
 
     }
-    public static void prim(ArrayList<Edge>graph[],int src){
-        boolean vis[]=new boolean[graph.length];
-        PriorityQueue<Pair>pq=new PriorityQueue<>();
-        int mstCost=0;
-        pq.add(new Pair(0,0));
-        while(!pq.isEmpty()){
-            Pair curr=pq.remove();
-            if(!vis[curr.n]){
-                vis[curr.n]=true;
-                mstCost+=curr.cost;
-                for (int i=0;i<graph[curr.n].size();i++){
-                    Edge e=graph[curr.n].get(i);
-                    pq.add(new Pair(e.dest,e.weight));
+
+    public static void prim(ArrayList<Edge> graph[], int src) {
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        int mstCost = 0;
+        pq.add(new Pair(0, 0));
+        while (!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if (!vis[curr.n]) {
+                vis[curr.n] = true;
+                mstCost += curr.cost;
+                for (int i = 0; i < graph[curr.n].size(); i++) {
+                    Edge e = graph[curr.n].get(i);
+                    pq.add(new Pair(e.dest, e.weight));
                 }
             }
         }
         System.out.println(mstCost);
     }
+
+    public static void primAlgo(ArrayList<Edge> graph[], int src) {
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        ArrayList<Edge> mstEdges = new ArrayList<>(); // Store edges in MST
+
+        pq.add(new Pair(src, 0));
+        int[] parent = new int[graph.length]; // Track parent of each node
+        parent[src] = -1; // Source has no parent
+ 
+        while (!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if (!vis[curr.n]) {
+                vis[curr.n] = true;
+
+                // If the node has a parent, add the corresponding edge to the MST
+                if (parent[curr.n] != -1) {
+                    mstEdges.add(new Edge(parent[curr.n], curr.n, curr.cost));
+                }
+
+                // Add all adjacent edges to the priority queue
+                for (int i = 0; i < graph[curr.n].size(); i++) {
+                    Edge e = graph[curr.n].get(i);
+                    if (!vis[e.dest]) {
+                        pq.add(new Pair(e.dest, e.weight));
+                        parent[e.dest] = curr.n; // Update parent for the destination
+                    }
+                }
+            }
+        }
+
+        // Output the edges of the MST
+        System.out.println("Edges in the MST:");
+        for (Edge edge : mstEdges) {
+            System.out.println("Src: " + edge.src + ", Dest: " + edge.dest + ", Weight: " + edge.weight);
+        }
+    }
+
     public static void main(String[] args) {
         ArrayList<Edge> graph[] = new ArrayList[4];
         createGraph(graph);
-        prim(graph,0);
+        primAlgo(graph, 0);
     }
 }
